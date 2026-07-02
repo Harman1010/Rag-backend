20f7eb2ed4c841c96c4af54abc16a5a3e80f480e
Harman1010 <hsabharwal5@gmail.com>
2026-07-02 13:11:41 +0530

    Update

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://ai-pdf-chatbot-mauve.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
